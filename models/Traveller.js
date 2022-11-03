4d7d28a5a6c410dbf48b83881da40a5380560a57
doldol123 <toruig59@gmail.com>
2022-11-03 11:28:16 -0700

    create Traveller model

--- models/Traveller.js
+++ models/Traveller.js
@@ -1,2 +1,32 @@
-const { Model, DataTypes } = require ('sequelize');
-const sequelize = require()
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Traveller extends Model { }
+
+Traveller.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'traveller',
+    }
+);
+
+module.exports = Traveller;
